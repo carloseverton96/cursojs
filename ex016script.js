566d533c30ad80e7c8b3eface4d7dc88a0c46fb8
carloseverton96 <140563844+carloseverton96@users.noreply.github.com>
2023-11-09 13:51:12 -0300

    4

--- ex016script.js
+++ ex016script.js
@@ -1,1 +1,33 @@
-alert('olá!')
+//neste exercício ao invés de usar o "var" está sendo usado o "let"
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementByTd('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar!'
+
+} else {
+    res.innerHTML = 'contando: <br>'
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    if (p <= 0) {
+        window.alert('Passo inválido! Considerando PASSO 1')
+        p = 1
+    }
+    if (i < f) {
+        //contagem crescente
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1f449}`
+        } 
+    } else {
+        //contagem regressiva
+        for(let c = i; c >= f; c -= p) {
+            res.innerHTML += `${c} \u{1F449}`
+        }
+    }
+    res.innerHTML += `\u{1F3c1}`
+    }
+}
